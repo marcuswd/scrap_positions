81b601dfea3c3a634a723c1f1974cff695450888
Marcus Vinicius <marcus@marcuswd.dev>
2024-09-04 20:47:56 -0300

    fix: Opção de busca de vagas no exterior ou Brasil, verifica se o Google solicitou captcha e nome de arquivo variável de acordo com o data da busca.

--- scrap.py
+++ scrap.py
@@ -1,5 +1,6 @@
 import openpyxl
 import time
+from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -10,23 +11,24 @@ driver = webdriver.Chrome()
 
 sources = []
 list_roles = []
-joblist = []
+total_vacancies = 0
 
-input_locale = input("Deseja buscar vagas no BR ou Exterior? (BR/EXT)")
+input_locale = input("Deseja buscar vagas no BR ou Exterior? (br/ext) [Deixe em branco para buscar no Exterior]")
 
 if input_locale == "BR":
-  sources_path = "br/sources.txt";
-  string_search = "%22remoto%22";
+  sources_path = "br/sources.txt"
+  string_search = "%22remoto%22"
 else :
   sources_path = "ext/sources.txt"
   string_search = "%22remote%22+-%22remote+in+the+US%22"
 
-input_start_date = input("Qual a data de início da busca? (AAAA-MM-DD)")
+input_start_date = input("Qual a data de início da busca? (AAAA-MM-DD) [Deixe em branco para buscar vagas dos últimos 7 dias]")
 
-if input_start_date != "" :
-  start_date = time.strftime(input_start_date)
-else :
-  start_date = time.strftime("%Y-%m-%d")
+if input_start_date != "":
+  start_date = input_start_date
+else:
+  today = datetime.now().date().strftime('%Y-%m-%d')
+  start_date = (datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d')
 
 workbook = openpyxl.Workbook()
 sheet = workbook.active
@@ -45,15 +47,23 @@ for role in list_roles:
   
   for source in sources:
     url = f"https://www.google.com/search?q=site:{source}+%22{role}%22+{string_search}+\"+after:{start_date}"
-    driver.get(url)
-    input("Please complete the CAPTCHA and then press Enter to continue...")
-    result_content = driver.find_elements(By.CSS_SELECTOR, '#search div > div h3')
-    if(result_content):
-      for position in result_content:
-        job_title = position.text
-        job_url = position.find_element(By.XPATH, '..').get_attribute('href')
-        current_tab.append([job_title, job_url])
+    url_result = driver.get(url)
+    if "https://www.google.com/sorry/index" in driver.current_url:
+      print("Please complete the CAPTCHA and then press Enter to continue...")
+      input()
+    else :
+      result_content = driver.find_elements(By.CSS_SELECTOR, '#search div > div h3')
+      if(result_content):
+        for position in result_content:
+          job_title = position.text
+          job_url = position.find_element(By.XPATH, '..').get_attribute('href')
+          current_tab.append([job_title, job_url])
+          total_vacancies += 1
 
-if 'Sheet' in workbook.sheetnames:
-  workbook.remove(workbook['Sheet'])
-workbook.save('jobs.xlsx')
+if(total_vacancies != 0):
+  if 'Sheet' in workbook.sheetnames:
+    workbook.remove(workbook['Sheet'])
+    workbook.save(f"vagas_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.xlsx")
+  else :
+    print("Nenhuma vaga encontrada")
+    driver.quit()
